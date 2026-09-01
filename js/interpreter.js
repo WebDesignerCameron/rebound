80428888204ed41e04b35c899472b567bd527121
WebDesignerCameron <webdesignercameron@gmail.com>
2026-09-01 09:21:28 +0100

    Add try-catch statement

--- js/interpreter.js
+++ js/interpreter.js
@@ -1,35 +1,39 @@
 /* Interpret a rebound file */
-document.getElementById("execute").addEventListener("click", ()=>{
-    /* Text and array variables */
-    const file = document.querySelector("input[type=file]").files[0];
-    const text = await file.text();
-    const lines = text.split(/\r?\n/);
-    let line;
-    let words;
-    let word;
-    /* Run-time variables */
-    let printMode = false;
-    let commentMode = false;
-    let output = "";
-    for(n1 = 0; n1 < lines.length; n1++){
-        line = lines[n1];
-        words = line.split(" ");
-        for(n2 = 0; n2 < words.length; n++){
-            if(!commentMode){
-                if(printMode){
-                    output += word;
-                }else if(word == "//"){
-                    commentMode = true;
-                }else if(word == "note"){
-                    printMode = true;
+try {
+    document.getElementById("execute").addEventListener("click", ()=>{
+        /* Text and array variables */
+        const file = document.querySelector("input[type=file]").files[0];
+        const text = await file.text();
+        const lines = text.split(/\r?\n/);
+        let line;
+        let words;
+        let word;
+        /* Run-time variables */
+        let printMode = false;
+        let commentMode = false;
+        let output = "";
+        for(n1 = 0; n1 < lines.length; n1++){
+            line = lines[n1];
+            words = line.split(" ");
+            for(n2 = 0; n2 < words.length; n++){
+                if(!commentMode){
+                    if(printMode){
+                        output += word;
+                    }else if(word == "//"){
+                        commentMode = true;
+                    }else if(word == "note"){
+                        printMode = true;
+                    }
+                }else{
+                    continue;
                 }
-            }else{
-                continue;
-            }
+            };
+            /* Reset variables */
+            printMode = false;
+            commentMode = false;
         };
-        /* Reset variables */
-        printMode = false;
-        commentMode = false;
-    };
-    document.getElementById("outputBox").textContent = output;
-});
+        document.getElementById("outputBox").textContent = output;
+    });
+}catch(error){
+    window.alert("An error has occured! It says: " + error.message);
+}
